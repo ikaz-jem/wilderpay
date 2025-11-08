70546c6402887bba500cbb2855d672743c0b384c
ikaz-jem <echchebabzakariae@gmail.com>
2025-11-08 20:14:33 +0100

    withdraw test

--- src/actions/withdraw/withdrawAction.js
+++ src/actions/withdraw/withdrawAction.js
@@ -9,40 +9,36 @@ import withdrawSchema from "../../app/models/withdrawSchema/withdrawSchema";
 import { withdrawUsdt } from "@/web3/transactions/withdrawUsdt.js";
 import { sendWithdrawEmail } from "./sendWithdrawEmail";
 
+const minWithdraw = 10;
 
-const minWithdraw = 10
+const minReff = 2;
 
+const minVolume = 50;
 
-const minReff = 2
+function calculateLeader(amount, volume, withdrawCount) {
+  const volumeMultiplier = volume > 500 ? 200 : 100;
+  const withdraw = (Number(withdrawCount) + 1) * 10;
+  const requiredVolume = (Number(withdrawCount) + 1) * volumeMultiplier;
+  const maxWithdraw = volume >= 1000 ? 100 : withdraw;
 
-const minVolume = 50
-
-
-function calculateLeader (amount ,volume,withdrawCount) {
-
-  const volumeMultiplier = volume>500 ? 200 : 100
-  const withdraw = (Number(withdrawCount)+1) * 10
-  const requiredVolume = (Number(withdrawCount)+1) * volumeMultiplier
-  const maxWithdraw = volume >= 1000 ? 100 : withdraw
-
-
-  if (amount > maxWithdraw){
-    return {success:false , message :`Max Withdraw For Your Level is ${maxWithdraw} USDT , Unlock More With Progress` }
+  if (amount > maxWithdraw) {
+    return {
+      success: false,
+      message: `Max Withdraw For Your Level is ${maxWithdraw} USDT , Unlock More With Progress`,
+    };
   }
 
-  if (volume < requiredVolume){
-    return {success:false , message :`You Need  ${requiredVolume} USDT in Total Volume For Your Level To Unlock Next Withdraw` }
+  if (volume < requiredVolume) {
+    return {
+      success: false,
+      message: `You Need  ${requiredVolume} USDT in Total Volume For Your Level To Unlock Next Withdraw`,
+    };
   }
 
-return {success:true}
-
+  return { success: true };
 }
 
-
-
-
 export async function withdrawAction(amount, toAddress, chain, apiKey) {
-
   const isValid = apiKey == process.env.NEXT_PUBLIC_SECRET;
 
   if (!isValid) {
@@ -60,15 +56,12 @@ export async function withdrawAction(amount, toAddress, chain, apiKey) {
 
   const user = await UserSchema.findById(session.user.id);
 
-
   if (user?.role == "demo") {
     return { success: false, message: "Not Allowed", type: "error" };
   }
 
   if (!user)
     return { success: false, message: "User Not Found", type: "error" };
-
-
 
   if (user.isRestricted)
     return {
@@ -84,67 +77,58 @@ export async function withdrawAction(amount, toAddress, chain, apiKey) {
       type: "error",
     };
 
-  
-     if (user.role === "leader" && user?.referredUsers?.length < minReff   ) {
+  if (user.role === "leader" && user?.referredUsers?.length < minReff) {
     return {
       success: false,
       message: `You Need At Least ${minReff} Direct Referrals To Unlock Withdrawals`,
       type: "error",
     };
   }
+
+  if (user.role == "leader") {
+    const check = calculateLeader(
+      amount,
+      user.totalVolume,
+      user?.withdrawls?.length
+    );
+    console.log(check);
+    if (!check.success) {
+      return {
+        success: false,
+        message: check?.message,
+        type: "error",
+      };
+    }
+  }
+  if (user.role == "admin") {
   
-
-
-
-     if (user.role == "leader" ) {
-
-     const check = calculateLeader(amount , user.totalVolume ,user?.withdrawls?.length )
-     console.log(check)
-if (!check.success){
-  return {
-    success: false,
-    message: check?.message,
-    type: "error",
-  };
-}
-
-}
-
-
-
-return {
-  success: false,
-  message: "success",
-  type: "error",
-};
-
-
-
-
+      return {
+        success: false,
+        message: "admins cannot withdraw",
+        type: "error",
+      };
   
-//  if (user.role === "leader" && user?.withdrawls?.length == 0 && user.totalVolume < minVolume && amount >maxWithdraw) {
-//     return {
-//       success: false,
-//       message: "Max Withdraw is 20$ , you need At Least 100$ Volume To unlock unlimited Amount",
-//       type: "error",
-//     };
-//   }
+  }
 
+ 
 
-  
-  
-  
-  
+  //  if (user.role === "leader" && user?.withdrawls?.length == 0 && user.totalVolume < minVolume && amount >maxWithdraw) {
+  //     return {
+  //       success: false,
+  //       message: "Max Withdraw is 20$ , you need At Least 100$ Volume To unlock unlimited Amount",
+  //       type: "error",
+  //     };
+  //   }
+
   const balance = await Balance.findOne({
     user: session.user.id,
     currency: "usdt",
   });
-  
-  
+
   if (balance.amount < amount) {
     return { success: false, message: "low balance" };
   }
-  
+
   if (amount < minWithdraw) {
     return { success: false, message: "Min Withdraw is 10 USDT" };
   }
@@ -157,9 +141,8 @@ return {
   //    };
   //  }
 
-
   try {
-   // adjust the path if needed
+    // adjust the path if needed
 
     const result = await withdrawUsdt({
       toAddress: toAddress,
@@ -168,8 +151,6 @@ return {
       amount: amount,
     });
 
-
-
     if (result?.success) {
       balance.amount -= amount;
       await balance.save();
@@ -177,12 +158,11 @@ return {
       const withdrawed = await withdrawSchema.create({
         amount,
         chain,
-        signature:result?.txHash,
-        currency:"usdt",
+        signature: result?.txHash,
+        currency: "usdt",
         address: toAddress,
         user: user._id,
       });
-
 
       await UserSchema.findByIdAndUpdate(
         user._id,
@@ -190,12 +170,19 @@ return {
         { new: true }
       );
 
-        const emailSent = await sendWithdrawEmail(user?.email,amount,toAddress,result?.txHash)
+      const emailSent = await sendWithdrawEmail(
+        user?.email,
+        amount,
+        toAddress,
+        result?.txHash
+      );
 
       return { success: true, message: `withdraw of ${amount} completed ! ` };
-    }else {
-      
-      return { success: false, message: `Something Went Wrong Please try Again later ` };
+    } else {
+      return {
+        success: false,
+        message: `Something Went Wrong Please try Again later `,
+      };
     }
   } catch (error) {
     console.error("Error importing or running transfer:", error);
